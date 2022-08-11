9cd53276362f16a3d7053eed2ab5547982c9f7a5
ugo.buonadonna <ugo@elliptic.co>
2022-08-11 13:22:18 +0200

    add tests for API docs documents

---
+++ src/__tests__/acceptance/docs.acceptance.ts
@@ -0,0 +1,26 @@
+import {Client, expect} from '@loopback/testlab';
+import {BlockchainRestApiApplication} from '../..';
+import {setupApplication} from './test-helper';
+
+describe('The API Docs HTML document', () => {
+  let app: BlockchainRestApiApplication;
+  let client: Client;
+
+  before('setupApplication', async () => {
+    ({app, client} = await setupApplication());
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('is reachable on /docs.html', async () => {
+    const res = await client.get('/docs.html').expect(200);
+    expect(res.text).to.containEql("openapi.json");
+  });
+
+  it('references a valid openapi.json file', async () => {
+    const res = await client.get('/openapi.json').expect(200);
+    expect(res.text).to.containEql(`"openapi": "3.0.0"`);
+  });
+});
