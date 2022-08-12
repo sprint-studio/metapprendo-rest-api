2f0455bc54f255a81949e2bf3a872c8101d6cc87
ugo.buonadonna <ugo@elliptic.co>
2022-08-12 20:59:58 +0200

    add challenge headers for browser prompt of basic auth

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -1,27 +1,35 @@
 // This basic auth middleware is temporary until we set up the service
 // in Kubernetes with mutual HTTPS verification and IP Whitelisting
-import {Middleware} from '@loopback/rest';
+import {Middleware, MiddlewareContext} from '@loopback/rest';
 import auth from 'basic-auth';
+
+
 
 const authMiddleware: Middleware = async (middlewareCtx, next) => {
     const {request} = middlewareCtx;
 
-    const credentials = auth(request)
-    
-    if(!credentials) {
-      return middlewareCtx.response.status(401).send()
+    const sendChallenge = (res: MiddlewareContext["response"]) => {
+      res.statusCode = 401
+      res.setHeader('WWW-Authenticate', 'Basic realm="Node"')
+      return res.end('Unauthorized')
     }
 
-    const credentialsAreCorrect = credentials.name === process.env.BASIC_AUTH_USERNAME 
+    const credentials = auth(request)
+
+    if(!credentials) {
+      return sendChallenge(middlewareCtx.response)
+    }
+
+    const credentialsAreCorrect = credentials.name === process.env.BASIC_AUTH_USERNAME
       && credentials.pass === process.env.BASIC_AUTH_PASSWORD
 
     if(!credentialsAreCorrect) {
-      return middlewareCtx.response.status(401).send()
+      return sendChallenge(middlewareCtx.response)
     }
 
     // Proceed with next middleware
     return next();
 
   };
-  
+
   export default authMiddleware;
