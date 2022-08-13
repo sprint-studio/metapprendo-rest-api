d262c74f18456f1584c98cf6813abc9b19707252
ugo.buonadonna <ugo@elliptic.co>
2022-08-13 13:27:59 +0200

    MET-5 add endpoints from 7 to 9

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,26 @@
+import {inject} from '@loopback/core';
+import {del, param, post, Request, requestBody, response, RestBindings} from '@loopback/rest';
+
+export class UsersController {
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+
+  @del('/users/{userId}')
+  @response(200)
+  deactivateUser(@param.path.string('userId') userId: string) {
+
+  }
+
+  @post('/users')
+  @response(200)
+  createUser(@requestBody() user: {}): {} {
+    return {}
+  }
+
+  @post('/users/{userId}/dossier')
+  @response(200)
+  updateUserDossier(
+    @param.path.string('userId') userId: string,
+    @requestBody() dossierUpdate: {}): {} {
+    return {}
+  }
+}
