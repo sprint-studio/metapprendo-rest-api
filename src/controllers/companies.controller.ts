d60b2adb80183790f27813fb8fb2c0281a22d693
Davide <thekage91@gmail.com>
2022-08-17 15:06:35 +0200

    MET-4 - Add Endpoint to return company admin

--- src/controllers/companies.controller.ts
+++ src/controllers/companies.controller.ts
@@ -1,35 +1,55 @@
-import {inject} from '@loopback/core';
-import {model, property} from '@loopback/repository';
-import {param, post, Request, requestBody, RestBindings} from '@loopback/rest';
-
+import { inject } from "@loopback/core";
+import { model, property } from "@loopback/repository";
+import {
+  param,
+  post,
+  get,
+  Request,
+  requestBody,
+  RestBindings,
+} from "@loopback/rest";
 
 @model()
 class CreateNewCompanyGroupAdminBody {
   @property({
-    type: 'string',
-    required: true
+    type: "string",
+    required: true,
   })
   idUtente: string;
 }
 export class CompaniesController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
-  @post('/companies/{companyId}/admin', {
-    description: "Autorizza un utente pre-esistente ad essere amministratore del gruppo aziendale",
+  @post("/companies/{companyId}/admin", {
+    description:
+      "Autorizza un utente pre-esistente ad essere amministratore del gruppo aziendale",
     responses: {
-      '200': {
-        description: "Utente promosso ad Admin del gruppo aziendale con successo",
+      "200": {
+        description:
+          "Utente promosso ad Admin del gruppo aziendale con successo",
       },
     },
   })
   createNewCompanyGroupAdmin(
-    @param.path.string('companyId') companyId: string,
+    @param.path.string("companyId") companyId: string,
     @requestBody({
-      description: 'a modified user',
+      description: "a modified user",
       required: true,
-    }) userId: CreateNewCompanyGroupAdminBody
+    })
+    userId: CreateNewCompanyGroupAdminBody
   ): {} {
-    return {
-    };
+    return {};
+  }
+
+  @get("/companies/{companyId}/admin", {
+    description: "Ritorna l'amministratore del gruppo aziendale",
+    responses: {
+      "200": {
+        description: "Admin del gruppo aziendale",
+      },
+    },
+  })
+  getCompanyGroupAdmin(@param.path.string("companyId") companyId: string): {} {
+    return {};
   }
 }
