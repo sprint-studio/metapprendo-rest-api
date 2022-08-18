385b02704cea2cac2b681f93bf3d9405a8039a92
Davide <thekage91@gmail.com>
2022-08-18 21:41:16 +0200

    MET-11 - Add request body and response for createCompanyEducationalAgenda endpoint

--- src/controllers/companyEducationalAgendas.controller.ts
+++ src/controllers/companyEducationalAgendas.controller.ts
@@ -1,12 +1,55 @@
-import {inject} from '@loopback/core';
-import {post, Request, requestBody, response, RestBindings} from '@loopback/rest';
+import { inject } from "@loopback/core";
+import { property } from "@loopback/repository";
+import {
+  post,
+  Request,
+  requestBody,
+  response,
+  RestBindings,
+} from "@loopback/rest";
 
+import { BlockchainTransaction } from "../models";
+
+class CreateCompanyEducationalAgendaBody {
+  @property({
+    type: "string",
+    required: true,
+  })
+  idTask: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  idPillola: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  idLavoratore: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  stato: string;
+}
 export class CompanyEducationalAgendasController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
-  @post('/company_educational_agendas')
+  @post("/company_educational_agendas")
   @response(200)
-  createCompanyEducationalAgenda(@requestBody() companyEducationalAgenda: {}): {} {
-    return {}
+  createCompanyEducationalAgenda(
+    @requestBody({
+      description: "Dettagli per creare un'agenda formativa aziendale",
+      required: true,
+    })
+    companyEducationalAgenda: CreateCompanyEducationalAgendaBody
+  ): {} {
+    return new BlockchainTransaction({
+      idTrx: "33423422342dsdfew",
+      dataOraTrx: new Date(),
+    });
   }
 }
