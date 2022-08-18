ff0dbafb847394949c5c7ed5c45338dc9f3e96e2
Davide <thekage91@gmail.com>
2022-08-18 21:24:26 +0200

    MET-11 - Add request body and response for createEducationalPill endpoint

--- src/controllers/educationalPills.controller.ts
+++ src/controllers/educationalPills.controller.ts
@@ -1,15 +1,37 @@
-import {inject} from '@loopback/core';
+import { inject } from "@loopback/core";
+import { property } from "@loopback/repository";
 import {
-  post, Request, requestBody, response, RestBindings
-} from '@loopback/rest';
+  post,
+  Request,
+  requestBody,
+  response,
+  RestBindings,
+} from "@loopback/rest";
+import { BlockchainTransaction } from "../models";
+
+class CreateEducationalPillBody {
+  @property({
+    type: "string",
+    required: true,
+  })
+  idPillola: string;
+}
 
 export class EducationalPillsController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
-  @post('/educational_pills')
+  @post("/educational_pills")
   @response(200, {})
-  createEducationalPill(@requestBody() educationalPill: {}): object {
-    return {
-    };
+  createEducationalPill(
+    @requestBody({
+      description: "I dettagli della pillola da dover creare",
+      required: true,
+    })
+    educationalPill: CreateEducationalPillBody
+  ): object {
+    return new BlockchainTransaction({
+      idTrx: "33423422342dsdfew",
+      dataOraTrx: new Date(),
+    });
   }
 }
