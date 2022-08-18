a4759d338ef0fe4a3a5fec239edd3d1edebedbb7
Davide <thekage91@gmail.com>
2022-08-18 22:11:51 +0200

    MET-11 - Add request body and response for createGroup endpoint

--- src/controllers/groups.controller.ts
+++ src/controllers/groups.controller.ts
@@ -1,15 +1,56 @@
-import {inject} from '@loopback/core';
+import { inject } from "@loopback/core";
+import { property } from "@loopback/repository";
 import {
-  post, Request, requestBody, response, RestBindings
-} from '@loopback/rest';
+  post,
+  Request,
+  requestBody,
+  response,
+  RestBindings,
+} from "@loopback/rest";
+
+import { BlockchainTransaction } from "../models";
+
+class CreateGroupBody {
+  @property({
+    type: "string",
+    required: true,
+  })
+  idGruppo: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  nome: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  descrizione: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  stato: string;
+}
 
 export class GroupsController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
-  @post('/groups')
+  @post("/groups")
   @response(200, {})
-  createGroup(@requestBody() group: {}): object {
-    return {
-    };
+  createGroup(
+    @requestBody({
+      description: "Dettagli per la creazione di un nuovo gruppo",
+      required: true,
+    })
+    group: CreateGroupBody
+  ): object {
+    return new BlockchainTransaction({
+      idTrx: "33423422342dsdfew",
+      dataOraTrx: new Date(),
+    });
   }
 }
--- src/controllers/profiles.controller.ts
+++ src/controllers/profiles.controller.ts
@@ -1,15 +1,55 @@
-import {inject} from '@loopback/core';
+import { inject } from "@loopback/core";
+import { property } from "@loopback/repository";
 import {
-  post, Request, requestBody, response, RestBindings
-} from '@loopback/rest';
+  post,
+  Request,
+  requestBody,
+  response,
+  RestBindings,
+} from "@loopback/rest";
 
+import { BlockchainTransaction } from "../models";
+
+class CreateProfileBody {
+  @property({
+    type: "string",
+    required: true,
+  })
+  idProfile: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  nome: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  descrizione: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  stato: string;
+}
 export class ProfilesController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
-  @post('/profiles')
+  @post("/profiles")
   @response(200, {})
-  createProfile(@requestBody() profile: {}): object {
-    return {
-    };
+  createProfile(
+    @requestBody({
+      description: "Dettagli per la creazione del nuovo profile ",
+      required: true,
+    })
+    profile: CreateProfileBody
+  ): object {
+    return new BlockchainTransaction({
+      idTrx: "33423422342dsdfew",
+      dataOraTrx: new Date(),
+    });
   }
 }
