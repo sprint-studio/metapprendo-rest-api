ff8dfaa4f33a20d6cea8e1f5cfe691087ccccf07
Davide <thekage91@gmail.com>
2022-08-18 22:04:33 +0200

    MET-12 - Add request body and response for createTrainingPath endpoint

--- src/controllers/trainingPaths.controller.ts
+++ src/controllers/trainingPaths.controller.ts
@@ -1,12 +1,50 @@
-import {inject} from '@loopback/core';
-import {post, Request, requestBody, response, RestBindings} from '@loopback/rest';
+import { inject } from "@loopback/core";
+import { property } from "@loopback/repository";
+import {
+  post,
+  Request,
+  requestBody,
+  response,
+  RestBindings,
+} from "@loopback/rest";
+
+import { BlockchainTransaction } from "../models";
+
+class CreateTrainingPathBody {
+  @property({
+    type: "string",
+    required: true,
+  })
+  idItem: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  idLavoratore: string;
+
+  @property({
+    type: "string",
+    required: true,
+  })
+  stato: string;
+}
 
 export class TrainingPathsController {
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
-  @post('/training_paths')
+  @post("/training_paths")
   @response(200)
-  createTrainingPath(@requestBody() trainingPath: {}): {} {
-    return {}
+  createTrainingPath(
+    @requestBody({
+      description: "Dettagli per la creazione del percorso di istruzione",
+      required: true,
+    })
+    trainingPath: CreateTrainingPathBody
+  ): {} {
+    return new BlockchainTransaction({
+      idTrx: "33423422342dsdfew",
+      dataOraTrx: new Date(),
+    });
   }
 }
